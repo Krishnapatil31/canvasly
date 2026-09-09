010329ac2b19fad298ae219544a33781f512dbd4
Krishnapatil31 <kp7259655@gmail.com>
2026-09-10 00:12:01 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.CANVASLY_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyA-zl41qVZqEvh6VdOz6XT7fWsJthSfMZI",
-  authDomain: "canvasly-800af.firebaseapp.com",
-  projectId: "canvasly-800af",
-  storageBucket: "canvasly-800af.firebasestorage.app",
-  messagingSenderId: "586191902956",
-  appId: "1:586191902956:web:0f3b542e127797c43b6dc9"
-};
